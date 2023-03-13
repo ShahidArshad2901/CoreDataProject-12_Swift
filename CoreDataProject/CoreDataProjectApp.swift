927f115256879f2d156d43afc50ee18b7f3f6435
Shahid Arshad <shahid.arshad@devsinc.com>
2023-03-13 08:04:06 -0400

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Dev on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
