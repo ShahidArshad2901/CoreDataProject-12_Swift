35046d4ceab960e362d1deaee770b3f76d970c9f
Shahid Arshad <shahid.arshad@devsinc.com>
2023-03-15 08:14:28 -0400

    Completed Project 12 Day 1

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Dev on 15/03/2023.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores{ description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Dev on 15/03/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
